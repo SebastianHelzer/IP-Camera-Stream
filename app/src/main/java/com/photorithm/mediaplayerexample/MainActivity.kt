5b18e58b9615df5119d917744488be693d22c893
Sebastian Helzer <sebastian@helzer.eu>
2018-12-17 14:51:52 -0700

    Create the MediaPlayer Service

--- app/src/main/java/com/photorithm/mediaplayerexample/MainActivity.kt
+++ app/src/main/java/com/photorithm/mediaplayerexample/MainActivity.kt
@@ -5,6 +5,9 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
+    // Made from following this guide:
+    // https://www.sitepoint.com/a-step-by-step-guide-to-building-an-android-audio-player-app/
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
