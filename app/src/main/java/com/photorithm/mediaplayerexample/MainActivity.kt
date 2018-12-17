87b9a7e8e32e74ebedcba80c08a062f85734442f
Sebastian Helzer <sebastian@helzer.eu>
2018-12-17 14:58:23 -0700

    Constructing the MediaPlayer

--- app/src/main/java/com/photorithm/mediaplayerexample/MainActivity.kt
+++ app/src/main/java/com/photorithm/mediaplayerexample/MainActivity.kt
@@ -3,10 +3,12 @@ package com.photorithm.mediaplayerexample
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
+
 class MainActivity : AppCompatActivity() {
 
     // Made from following this guide:
     // https://www.sitepoint.com/a-step-by-step-guide-to-building-an-android-audio-player-app/
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/photorithm/mediaplayerexample/MediaPlayerService.kt
+++ app/src/main/java/com/photorithm/mediaplayerexample/MediaPlayerService.kt
@@ -6,6 +6,8 @@ import android.content.Intent
 import android.os.IBinder
 import android.media.AudioManager
 import android.os.Binder
+import android.util.Log
+import java.io.IOException
 
 
 /*
@@ -15,6 +17,70 @@ class MediaPlayerService : Service(), MediaPlayer.OnCompletionListener, MediaPla
     MediaPlayer.OnErrorListener, MediaPlayer.OnSeekCompleteListener, MediaPlayer.OnInfoListener,
     MediaPlayer.OnBufferingUpdateListener, AudioManager.OnAudioFocusChangeListener {
 
+    private var mediaPlayer: MediaPlayer? = null
+    //path to the audio file
+    private val mediaFile: String? = null
+
+    private fun initMediaPlayer() {
+        mediaPlayer = MediaPlayer()
+        //Set up MediaPlayer event listeners
+        mediaPlayer?.setOnCompletionListener(this)
+        mediaPlayer?.setOnErrorListener(this)
+        mediaPlayer?.setOnPreparedListener(this)
+        mediaPlayer?.setOnBufferingUpdateListener(this)
+        mediaPlayer?.setOnSeekCompleteListener(this)
+        mediaPlayer?.setOnInfoListener(this)
+        //Reset so that the MediaPlayer is not pointing to another data source
+        mediaPlayer?.reset()
+
+        mediaPlayer?.setAudioStreamType(AudioManager.STREAM_MUSIC)
+        try {
+            // Set the data source to the mediaFile location
+            mediaPlayer?.setDataSource(mediaFile)
+        } catch (e: IOException) {
+            e.printStackTrace()
+            stopSelf()
+        }
+
+        mediaPlayer?.prepareAsync()
+    }
+
+    //Used to pause/resume MediaPlayer
+    private var resumePosition: Int = 0
+
+    private fun playMedia() {
+        if(mediaPlayer != null) {
+            if (!mediaPlayer!!.isPlaying) {
+                mediaPlayer!!.start()
+            }
+        }
+    }
+
+    private fun stopMedia() {
+        if (mediaPlayer != null) {
+            if (mediaPlayer!!.isPlaying) {
+                mediaPlayer!!.stop()
+            }
+        }
+    }
+
+    private fun pauseMedia() {
+        if (mediaPlayer != null) {
+            if (mediaPlayer!!.isPlaying) {
+                mediaPlayer!!.pause()
+                resumePosition = mediaPlayer!!.getCurrentPosition()
+            }
+        }
+    }
+
+    private fun resumeMedia() {
+        if (mediaPlayer != null) {
+            if (!mediaPlayer!!.isPlaying) {
+                mediaPlayer!!.seekTo(resumePosition)
+                mediaPlayer!!.start()
+            }
+        }
+    }
 
     // Binder given to clients
     private val iBinder = LocalBinder()
@@ -30,11 +96,22 @@ class MediaPlayerService : Service(), MediaPlayer.OnCompletionListener, MediaPla
 
     override fun onCompletion(mp: MediaPlayer) {
         //Invoked when playback of a media source has completed.
+        stopMedia()
+        //stop the service
+        stopSelf()
     }
 
     //Handle errors
     override fun onError(mp: MediaPlayer, what: Int, extra: Int): Boolean {
-        //Invoked when there has been an error during an asynchronous operation.
+        //Invoked when there has been an error during an asynchronous operation
+        when (what) {
+            MediaPlayer.MEDIA_ERROR_NOT_VALID_FOR_PROGRESSIVE_PLAYBACK -> Log.d(
+                "MediaPlayer Error",
+                "MEDIA ERROR NOT VALID FOR PROGRESSIVE PLAYBACK $extra"
+            )
+            MediaPlayer.MEDIA_ERROR_SERVER_DIED -> Log.d("MediaPlayer Error", "MEDIA ERROR SERVER DIED $extra")
+            MediaPlayer.MEDIA_ERROR_UNKNOWN -> Log.d("MediaPlayer Error", "MEDIA ERROR UNKNOWN $extra")
+        }
         return false
     }
 
@@ -45,6 +122,7 @@ class MediaPlayerService : Service(), MediaPlayer.OnCompletionListener, MediaPla
 
     override fun onPrepared(mp: MediaPlayer) {
         //Invoked when the media source is ready for playback.
+        playMedia()
     }
 
     override fun onSeekComplete(mp: MediaPlayer) {
