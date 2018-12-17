f6461a57c3ea4fd142c344e9d64c2fb12efed847
Sebastian Helzer <sebastian@helzer.eu>
2018-12-17 16:07:31 -0700

    Play from storage

---
+++ app/src/main/java/com/photorithm/mediaplayerexample/Audio.kt
@@ -0,0 +1,6 @@
+package com.photorithm.mediaplayerexample
+
+/*
+ * Created by Sebastian Helzer on 12/17/2018.
+ */
+class Audio(var data: String?, var title: String?, var album: String?, var artist: String?)
--- app/src/main/java/com/photorithm/mediaplayerexample/MainActivity.kt
+++ app/src/main/java/com/photorithm/mediaplayerexample/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.photorithm.mediaplayerexample
 
+import android.Manifest
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.content.ComponentName
@@ -8,6 +9,11 @@ import android.widget.Toast
 import android.os.IBinder
 import android.content.ServiceConnection
 import android.content.Intent
+import android.provider.MediaStore
+import android.content.ContentResolver
+import android.content.pm.PackageManager
+import android.os.Build
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,6 +21,33 @@ class MainActivity : AppCompatActivity() {
     // https://www.sitepoint.com/a-step-by-step-guide-to-building-an-android-audio-player-app/
     private var player: MediaPlayerService? = null
     var serviceBound = false
+    var audioList: ArrayList<Audio>? = null
+    private val MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 50000
+
+
+    private fun loadAudio() {
+
+
+        val contentResolver = contentResolver
+        val uri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI
+        val selection = MediaStore.Audio.Media.IS_MUSIC + "!= 0"
+        val sortOrder = MediaStore.Audio.Media.TITLE + " ASC"
+        val cursor = contentResolver.query(uri, null, selection, null, sortOrder)
+
+        if (cursor != null && cursor.count > 0) {
+            audioList = ArrayList()
+            while (cursor.moveToNext()) {
+                val data = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.DATA))
+                val title = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.TITLE))
+                val album = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.ALBUM))
+                val artist = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.ARTIST))
+
+                // Save to audioList
+                audioList!!.add(Audio(data, title, album, artist))
+            }
+        }
+        cursor!!.close()
+    }
 
     //Binding this Client to the AudioPlayer Service
     private val serviceConnection = object : ServiceConnection {
@@ -67,7 +100,40 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        checkIfCanLoadAudio()
+        //play the first audio in the ArrayList
+        if (audioList != null && audioList!!.isNotEmpty()) {
+            playAudio(audioList!![0].data!!)
+        } else {
+            playAudio("https://upload.wikimedia.org/wikipedia/commons/6/6c/Grieg_Lyric_Pieces_Kobold.ogg")
+        }
+    }
 
-        playAudio("https://upload.wikimedia.org/wikipedia/commons/6/6c/Grieg_Lyric_Pieces_Kobold.ogg")
+    private fun checkIfCanLoadAudio() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if(checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                // Should we show an explanation?
+                if (shouldShowRequestPermissionRationale(Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                    // Explain to the user why we need to read the contacts
+                }
+
+                requestPermissions(
+                    arrayOf( Manifest.permission.READ_EXTERNAL_STORAGE),
+                    MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE
+                );
+            } else {
+                loadAudio()
+            }
+        } else {
+            loadAudio()
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        if(permissions.isNotEmpty()){
+            if(requestCode == MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE){
+                loadAudio()
+            }
+        }
     }
 }
