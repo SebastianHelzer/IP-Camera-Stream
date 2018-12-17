ee3c2a1742c94857d0faed744aaee84a887bcb42
Sebastian Helzer <sebastian@helzer.eu>
2018-12-17 15:17:01 -0700

    Binding the Audio Player

--- app/src/main/java/com/photorithm/mediaplayerexample/MainActivity.kt
+++ app/src/main/java/com/photorithm/mediaplayerexample/MainActivity.kt
@@ -2,16 +2,53 @@ package com.photorithm.mediaplayerexample
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-
+import android.content.ComponentName
+import android.content.Context
+import android.widget.Toast
+import android.os.IBinder
+import android.content.ServiceConnection
+import android.content.Intent
 
 class MainActivity : AppCompatActivity() {
 
     // Made from following this guide:
     // https://www.sitepoint.com/a-step-by-step-guide-to-building-an-android-audio-player-app/
+    private var player: MediaPlayerService? = null
+    var serviceBound = false
 
+    //Binding this Client to the AudioPlayer Service
+    private val serviceConnection = object : ServiceConnection {
+        override fun onServiceConnected(name: ComponentName, service: IBinder) {
+            // We've bound to LocalService, cast the IBinder and get LocalService instance
+            val binder = service as MediaPlayerService.LocalBinder
+            player = binder.service
+            serviceBound = true
+
+            Toast.makeText(this@MainActivity, "Service Bound", Toast.LENGTH_SHORT).show()
+        }
+
+        override fun onServiceDisconnected(name: ComponentName) {
+            serviceBound = false
+        }
+    }
+
+    private fun playAudio(media: String) {
+        //Check is service is active
+        if (!serviceBound) {
+            val playerIntent = Intent(this, MediaPlayerService::class.java)
+            playerIntent.putExtra("media", media)
+            startService(playerIntent)
+            bindService(playerIntent, serviceConnection, Context.BIND_AUTO_CREATE)
+        } else {
+            //Service is active
+            //Send media with BroadcastReceiver
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        playAudio("https://upload.wikimedia.org/wikipedia/commons/6/6c/Grieg_Lyric_Pieces_Kobold.ogg")
     }
 }
